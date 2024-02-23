4084abe2cf1728b7ea465d705f48a1d9db6f0f88
Long Ngo <126145913+NVCLong@users.noreply.github.com>
2024-02-23 11:37:00 +0700

    Fix register function

--- src/main/java/com/rs/retailstore/model/Customer.java
+++ src/main/java/com/rs/retailstore/model/Customer.java
@@ -1,14 +1,19 @@
 package com.rs.retailstore.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private long id;
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String username;
     private String password;
--- src/main/java/com/rs/retailstore/service/CustomerService.java
+++ src/main/java/com/rs/retailstore/service/CustomerService.java
@@ -6,7 +6,11 @@ import com.rs.retailstore.respository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
 
 @Service
 public class CustomerService  {
@@ -15,18 +19,24 @@ public class CustomerService  {
 
     @Autowired
     JwtService jwtService;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
 
     public AuthenticationResponse createCustomer(Customer request) {
+        Random random = new Random();
         System.out.println(request);
         System.out.println(customerRepository.findByUsername("admin"));
         if(!customerRepository.findByUsername(request.getUsername()).isEmpty()) {
             return new AuthenticationResponse(null, "User is already created");
         }
         Customer customer= new Customer();
+
         customer.setUsername(request.getUsername());
-        customer.setPassword(request.getPassword());
+        customer.setPassword(passwordEncoder.encode(request.getPassword()));
         customer.setRole(request.getRole());
+        customer.setId(random.nextLong(0,100000));
+        System.out.println(customer);
         customerRepository.save(customer);
         return new AuthenticationResponse("Create user successfully with the Username "+ customer.getUsername());
 
